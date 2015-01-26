e18c7da80bd023a0d9c708bc0d04b603b41cc634
superfury <superfury@live.nl>
2015-01-26 11:06:44 +0000

    - zalloc header fixed for the new functionality.

--- headers/support/zalloc.h
+++ headers/support/zalloc.h
@@ -7,8 +7,7 @@ typedef void (*DEALLOCFUNC)(void **ptr, uint_32 size); //Deallocation functional
 OPTINLINE void *nzalloc(uint_32 size, char *name); //Allocates memory, NULL on failure (ran out of memory), protected allocation!
 OPTINLINE void *zalloc(uint_32 size, char *name); //Same as nzalloc, but clears the allocated memory!
 void freez(void **ptr, uint_32 size, char *name); //Release memory allocated with zalloc!
-void unregisterptrall(); //Free all allocated memory still allocated (on shutdown only, garbage collector)!
-void freezall(); //Same as unregisterall
+void freezall(); //Free all allocated memory still allocated (on shutdown only, garbage collector)!
 
 //Pointer derefenence protection!
 void *memprotect(void *ptr, uint_32 size, char *name); //Checks address (with name optionally) of pointer!
@@ -19,8 +18,7 @@ OPTINLINE uint_32 freemem(); //Free memory left!
 //Debugging functionality for finding memory leaks!
 void logpointers(char *cause); //Logs any changes in memory usage!
 
-//For stuff using external allocations:
-int registerptr(void *ptr,uint_32 size, char *name, DEALLOCFUNC dealloc); //Register a pointer!
-void unregisterptr(void *ptr, uint_32 size); //Remove pointer from registration (only if original pointer)?
-
+//For stuff using external allocations. Result: 1 on success, 0 on failure.
+byte registerptr(void *ptr,uint_32 size, char *name, DEALLOCFUNC dealloc); //Register a pointer!
+byte unregisterptr(void *ptr, uint_32 size); //Remove pointer from registration (only if original pointer)?
 #endif
