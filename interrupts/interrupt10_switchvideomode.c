9834060826bfee39738e22ad2731c10ff14cfd43
superfury <superfury@live.nl>
2015-03-23 13:53:22 +0000

    interrupt10_switchvideomode.c: Should fix odd/even bug.

--- interrupts/interrupt10_switchvideomode.c
+++ interrupts/interrupt10_switchvideomode.c
@@ -302,6 +302,7 @@ int INT10_Internal_SetVideoMode(word mode)
 	case M_CGA2:
 		seq_data[2]|=0xf;				//Enable plane 0
 		if (machine==MCH_EGA) seq_data[4]|=0x04;		//odd/even enabled
+		if (IS_VGA_ARCH) seq_data[4]|=0x04; //odd/even enabled
 		break;
 	case M_CGA4:
 		seq_data[2]|=0x03;		//Enable plane 0 and 1
