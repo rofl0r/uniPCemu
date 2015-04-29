e8d02169472ef35e3c58559a7a0dfd18696b50fd
superfury <superfury@live.nl>
2015-04-29 07:41:09 +0000

    adsr.c: Changed to basic ADSR.

--- hardware/midi/adsr.c
+++ hardware/midi/adsr.c
@@ -1,92 +1,92 @@
 #include "headers/types.h" //Basic types!
 #include "headers/hardware/midi/adsr.h" //Our own typedefs!
 
-void ADSR_release(ADSR *voice)
+void ADSR_release(ADSR *adsr)
 {
-	if (voice->release) //Gotten release?
+	if (adsr->release) //Gotten release?
 	{
-		voice->ADSREnvelope -= voice->releasefactor; //Apply factor!
-		if (voice->ADSREnvelope>0.0f) return; //Not quiet yet?
+		adsr->ADSREnvelope -= voice->releasefactor; //Apply factor!
+		if (adsr->ADSREnvelope>0.0f) return; //Not quiet yet?
 	}
-	voice->ADSREnvelope = 0.0f; //Nothing to sound!
-	voice->active = MIDISTATUS_IDLE; //Return to IDLE!
+	adsr->ADSREnvelope = 0.0f; //Nothing to sound!
+	adsr->active = MIDISTATUS_IDLE; //Return to IDLE!
 }
 
 void ADSR_sustain(ADSR *voice)
 {
-	/*if (voice->sustain) //Gotten sustain?
+	/*if (adsr->sustain) //Gotten sustain?
 	{*/
-	if ((voice->channel->sustain) || ((voice->currentloopflags & 0xC0) != 0x80)) //Disable our voice when not sustaining anymore!
+	if ((adsr->channel->sustain) || ((adsr->currentloopflags & 0xC0) != 0x80)) //Disable our voice when not sustaining anymore!
 	{
 		return; //Sustaining!
 	}
 	//}
 	//Sustain expired?
-	voice->active = MIDISTATUS_RELEASE; //Check next step!
-	voice->releasestart = voice->play_counter; //When we start to release!
-	ADSR_release(voice); //Passthrough!
+	adsr->active = MIDISTATUS_RELEASE; //Check next step!
+	adsr->releasestart = adsr->play_counter; //When we start to release!
+	ADSR_release(adsr); //Passthrough!
 }
 
-void ADSR_decay(ADSR *voice)
+void ADSR_decay(ADSR *adsr)
 {
-	if (voice->decay) //Gotten decay?
+	if (adsr->decay) //Gotten decay?
 	{
-		if (voice->decayend > voice->play_counter) //Decay busy?
+		if (adsr->decayend > adsr->play_counter) //Decay busy?
 		{
-			voice->ADSREnvelope -= voice->decayfactor; //Apply factor!
+			adsr->ADSREnvelope -= adsr->decayfactor; //Apply factor!
 			return; //Hold!
 		}
 	}
 	//Decay expired?
-	voice->active = MIDISTATUS_SUSTAIN; //Check next step!
-	voice->ADSREnvelope = voice->sustainfactor; //Apply sustain factor!
-	ADSR_sustain(voice); //Passthrough!
+	adsr->active = MIDISTATUS_SUSTAIN; //Check next step!
+	adsr->ADSREnvelope = adsr->sustainfactor; //Apply sustain factor!
+	ADSR_sustain(adsr); //Passthrough!
 }
 
-void ADSR_hold(ADSR *voice)
+void ADSR_hold(ADSR *adsr)
 {
-	if (voice->hold) //Gotten hold?
+	if (adsr->hold) //Gotten hold?
 	{
-		if (voice->holdend > voice->play_counter) //Hold busy?
+		if (adsr->holdend > adsr->play_counter) //Hold busy?
 		{
 			return; //Hold!
 		}
 	}
 	//Hold expired?
-	voice->active = MIDISTATUS_DECAY; //Check next step!
-	ADSR_decay(voice); //Passthrough!
+	adsr->active = MIDISTATUS_DECAY; //Check next step!
+	ADSR_decay(adsr); //Passthrough!
 }
 
-void ADSR_attack(ADSR *voice)
+void ADSR_attack(ADSR *adsr)
 {
-	if (voice->attack) //Gotten attack?
+	if (adsr->attack) //Gotten attack?
 	{
-		if (voice->attackend > voice->play_counter) //Attack busy?
+		if (adsr->attackend > adsr->play_counter) //Attack busy?
 		{
-			voice->ADSREnvelope += voice->attackfactor; //Apply factor!
+			adsr->ADSREnvelope += adsr->attackfactor; //Apply factor!
 			return;
 		}
 	}
 	//Attack expired?
-	voice->ADSREnvelope = 1.0f; //Make sure we're at 100%
-	voice->active = MIDISTATUS_HOLD; //Check next step!
-	ADSR_hold(voice); //Passthrough!
+	adsr->ADSREnvelope = 1.0f; //Make sure we're at 100%
+	adsr->active = MIDISTATUS_HOLD; //Check next step!
+	ADSR_hold(adsr); //Passthrough!
 }
 
-void ADSR_delay(ADSR *voice)
+void ADSR_delay(ADSR *adsr)
 {
-	if (voice->delaytime) //Gotten delay?
+	if (adsr->delaytime) //Gotten delay?
 	{
-		if (voice->delaytime > voice->play_counter) //Delay busy?
+		if (adsr->delaytime > adsr->play_counter) //Delay busy?
 		{
 			return; //Normal delay!
 		}
 	}
-	voice->active = MIDISTATUS_ATTACK; //Check next step!
-	ADSR_attack(voice); //Passthrough!
+	adsr->active = MIDISTATUS_ATTACK; //Check next step!
+	ADSR_attack(adsr); //Passthrough!
 }
 
-void ADSR_idle(ADSR *voice)
+void ADSR_idle(ADSR *adsr)
 {
 	//Idle does nothing!
 }
@@ -97,3 +97,11 @@ static MIDI_ADSR ADSR[7] = {
 	ADSR_sustain, //Holding/sustain
 	ADSR_release //Release
 }; //ADSR states!
+
+void ADSR_init(ADSR *adsr)
+{
+}
+
+void ADSR_tick(ADSR *adsr)
+{
+}
