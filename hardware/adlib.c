398453aaa8abf4da52790b2921d73e77ea04299b
superfury <superfury@live.nl>
2015-06-02 13:19:52 +0200

    Updated the adlib sound card emulation with two operators. It seems to give the wrong signal after 0.25 seconds. This still has to be fixed (wrong signal until about 30 seconds - 1 minute, after which the faulty signal disappears).

--- hardware/adlib.c
+++ hardware/adlib.c
@@ -37,26 +37,19 @@
 #define __HW_DISABLED 0
 //Use the adlib sound? If disabled, only run timers for the CPU. Sound will not actually be heard.
 #define __SOUND_ADLIB 1
-//Ignore ADSR?
-//#define __IGNORE_ADSR 1.0f
 //What volume, in percent!
 #define ADLIB_VOLUME 100.0f
+
+//Maximum DB volume (equalling 100% volume)!
+#define __MAX_DB 100.0f
 
 //extern void set_port_write_redirector (uint16_t startport, uint16_t endport, void *callback);
 //extern void set_port_read_redirector (uint16_t startport, uint16_t endport, void *callback);
 
 uint16_t baseport = 0x388; //Adlib address(w)/status(r) port, +1=Data port (write only)
-float usesamplerate = 49716.0; //The sample rate to use for output!
+float usesamplerate = 14318180.0f/288.0f; //The sample rate to use for output!
 
-uint8_t optable[0x16] = { 0, 0, 0, 1, 1, 1, 255, 255, 0, 0, 0, 1, 1, 1, 255, 255, 0, 0, 0, 1, 1, 1 };
 uint16_t adlibregmem[0xFF], adlibaddr = 0;
-
-int8_t waveform[4][64] = {
-	{ 1, 8, 13, 20, 26, 31, 37, 41, 47, 49, 54, 58, 58, 62, 63, 63, 64, 63, 62, 61, 58, 55, 52, 47, 45, 38, 34, 27, 23, 17, 10, 4,-2,-8,-15,-21,-26,-34,-36,-42,-48,-51,-54,-59,-60,-62,-64,-65,-65,-63,-64,-61,-59,-56,-53,-48,-46,-39,-36,-28,-24,-17,-11,-6 },
-	{ 1, 8, 13, 20, 25, 32, 36, 42, 46, 50, 54, 57, 60, 61, 62, 64, 63, 65, 61, 61, 58, 55, 51, 49, 44, 38, 34, 28, 23, 16, 11, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-	{ 1, 8, 13, 21, 25, 31, 36, 43, 45, 50, 54, 57, 59, 62, 63, 63, 63, 64, 63, 59, 59, 55, 52, 48, 44, 38, 34, 28, 23, 16, 10, 4, 2, 7, 14, 20, 26, 31, 36, 42, 45, 51, 54, 56, 60, 62, 62, 63, 65, 63, 62, 60, 58, 55, 52, 48, 44, 38, 34, 28, 23, 17, 10, 3 },
-	{ 1, 8, 13, 20, 26, 31, 36, 42, 46, 51, 53, 57, 60, 62, 61, 66, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 13, 21, 25, 32, 36, 41, 47, 50, 54, 56, 60, 62, 61, 67, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-};
 
 int8_t oplwave[4][256] = {
 	{
@@ -103,26 +96,51 @@ int8_t oplwave[4][256] = {
 
 };
 
-uint8_t oplstep[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+byte adliboperators[2][9] = { //Groupings of 22 registers! (20,40,60,80,E0)
+	{ 0x00, 0x01, 0x02, 0x08, 0x09, 0x0A, 0x10, 0x11, 0x12 },
+	{ 0x03, 0x04, 0x05, 0x0B, 0x0C, 0x0D, 0x13, 0x14, 0x15 }
+};
+
+byte adliboperatorsreverse[0x16] = { 0, 1, 2, 0, 1, 2,
+									255, 255,
+									3, 4, 5, 3, 4, 5,
+									255, 255,
+									6, 7, 8, 6, 7, 8}; //Channel lookup of adlib operators!
+
+byte wavemask = 0; //Wave select mask!
 
 struct structadlibop {
-	uint8_t wave;
-} adlibop[9][2];
+	uint8_t wavesel;
+	uint8_t AM; //Amplitude modulation enabled?
+	float ModulatorFrequencyMultiple; //What harmonic to sound?
+} adlibop[0x10];
 
 struct structadlibchan {
 	uint16_t freq;
 	double convfreq;
 	uint8_t keyon;
 	uint16_t octave;
-	uint8_t wavesel;
+	uint8_t synthmode; //What synthesizer mode (1=Additive synthesis, 0=Frequency modulation)
 } adlibch[0x10];
 
 double attacktable[0x10] = { 1.0003, 1.00025, 1.0002, 1.00015, 1.0001, 1.00009, 1.00008, 1.00007, 1.00006, 1.00005, 1.00004, 1.00003, 1.00002, 1.00001, 1.000005 }; //1.003, 1.05, 1.01, 1.015, 1.02, 1.025, 1.03, 1.035, 1.04, 1.045, 1.05, 1.055, 1.06, 1.065, 1.07, 1.075 };
 double decaytable[0x10] = { 0.99999, 0.999985, 0.99998, 0.999975, 0.99997, 0.999965, 0.99996, 0.999955, 0.99995, 0.999945, 0.99994, 0.999935, 0.99994, 0.999925, 0.99992, 0.99991 };
-double adlibenv[0x10], adlibdecay[0x10], adlibattack[0x10];
-uint8_t adlibdidattack[0x10], adlibpercussion = 0, adlibstatus = 0;
+double adlibenv[0x20], adlibdecay[0x20], adlibattack[0x20];
+uint8_t adlibdidattack[0x20], adlibpercussion = 0, adlibstatus = 0;
+
+float AMDepth = 0.0f; //AM depth in dB!
 
 uint16_t adlibport = 0x388;
+
+OPTINLINE double dB2factor(double dB, double fMaxLevelDB)
+{
+	return pow(10, ((dB - fMaxLevelDB) / 20));
+}
+
+OPTINLINE float calcModulatorFrequencyMultiple(byte data)
+{
+	return 1.0f; //Just ignore for now!
+}
 
 void outadlib (uint16_t portnum, uint8_t value) {
 	if (portnum==adlibport) {
@@ -132,6 +150,9 @@ void outadlib (uint16_t portnum, uint8_t value) {
 	portnum = adlibaddr;
 	adlibregmem[portnum] = value;
 	switch (portnum) {
+	case 0: //Waveform select enable
+		wavemask = (adlibregmem[0] & 0x20) ? 3 : 0; //Apply waveform mask!
+		break;
 			case 4: //timer control
 				if (value&0x80) {
 						adlibstatus = 0;
@@ -141,27 +162,42 @@ void outadlib (uint16_t portnum, uint8_t value) {
 			case 0xBD:
 				if (value & 0x10) adlibpercussion = 1;
 				else adlibpercussion = 0;
+				AMDepth = (value & 0x80) ? dB2factor(4.8f, __MAX_DB) : dB2factor(1.0f, __MAX_DB); //AM depth in dB!
 				break;
 		}
-	if ( (portnum >= 0x60) && (portnum <= 0x75) ) { //attack/decay
-			portnum &= 15;
+	if ((portnum >= 0x20) && (portnum <= 0x35)) //Various flags
+	{
+		portnum &= 0x1F;
+		adlibop[portnum].AM = (adlibregmem[0x20 + portnum] & 0x80) ? 1 : 0; //Take the AM bit!
+		adlibop[portnum].ModulatorFrequencyMultiple = calcModulatorFrequencyMultiple(adlibregmem[0x20 + portnum] & 0xF); //Which harmonic to use?
+	}
+	else if ( (portnum >= 0x60) && (portnum <= 0x75) ) { //attack/decay
+			portnum &= 0x1F;
 			adlibattack[portnum] = attacktable[15- (value>>4) ]*1.006;
 			adlibdecay[portnum] = decaytable[value&15];
 		}
 	else if ( (portnum >= 0xA0) && (portnum <= 0xB8) ) { //octave, freq, key on
-			portnum &= 15;
+		if (portnum & 0x8) return; //Ignore A8-AF!
+			portnum &= 0x7; //Only 9 channels
 			if (!adlibch[portnum].keyon && ( (adlibregmem[0xB0+portnum]>>5) &1) ) {
-					adlibdidattack[portnum] = 0;
-					adlibenv[portnum] = 0.0025;
-				}
+					adlibdidattack[adliboperators[0][portnum]] = 0;
+					adlibdidattack[adliboperators[1][portnum]] = 0;
+					adlibenv[adliboperators[0][portnum]] = 0.0025;
+					adlibenv[adliboperators[1][portnum]] = 0.0025;
+			}
 			adlibch[portnum].freq = adlibregmem[0xA0+portnum] | ( (adlibregmem[0xB0+portnum]&3) <<8);
 			adlibch[portnum].convfreq = ( (double) adlibch[portnum].freq * 0.7626459);
 			adlibch[portnum].keyon = (adlibregmem[0xB0+portnum]>>5) &1;
 			adlibch[portnum].octave = (adlibregmem[0xB0+portnum]>>2) &7;
 		}
+	else if ((portnum >= 0xC0) && (portnum <= 0xC8))
+	{
+		portnum &= 7;
+		adlibch[portnum].synthmode = (adlibregmem[0xC0 + portnum] & 1); //Save the synthesis mode!
+	}
 	else if ( (portnum >= 0xE0) && (portnum <= 0xF5) ) { //waveform select
-			portnum &= 15;
-			if (portnum<9) adlibch[portnum].wavesel = value&3;
+			portnum &= 0x1F;
+			adlibop[portnum].wavesel = value&3;
 		}
 }
 
@@ -175,6 +211,7 @@ uint8_t inadlib (uint16_t portnum) {
 uint16_t adlibfreq (uint8_t chan) {
 	//uint8_t downoct[4] = { 3, 2, 1, 0 };
 	//uint8_t upoct[3] = { 1, 2, 3 };
+	if (chan > 8) return (0); //Invalid channel: we only have 9 channels!
 	uint16_t tmpfreq;
 	if (!adlibch[chan].keyon) return (0);
 	tmpfreq = (uint16_t) adlibch[chan].convfreq;
@@ -207,63 +244,97 @@ uint16_t adlibfreq (uint8_t chan) {
 	return (tmpfreq);
 }
 
-uint64_t fullstep, adlibstep[0x10];
-
 //Original again!
 OPTINLINE char adlibsample (uint8_t curchan) {
-	char tempsample;
-	float tempstep;
+	static uint64_t adlibstep[0x20];
+	float fullstep1, fullstep2, tempsample, result, tempstep, effectivemodulator, effectivecarrier;
+	byte operator1, operator2; //What operator is our modulator and carrier?
+
 	if (curchan >= NUMITEMS(adlibch)) return 0; //No sample with invalid channel!
-
-	fullstep = usesamplerate/adlibfreq (curchan);
-
-	tempsample = oplwave[adlibch[curchan].wavesel][ (uint8_t) ( (double) adlibstep[curchan]++/ ( (double) fullstep/ (double)256) ) ];
-	if (adlibpercussion && (curchan>=6) && (curchan<=8) ) //We're percussion?
+	if (adlibpercussion && (curchan >= 6) && (curchan <= 8)) //We're percussion?
 	{
-		//We use a noise generator! Added by superfury1
-		tempsample = RandomShort(-116,116); //Generate a random sample between -MAX and +MAX!
+		return 0; //Percussion isn't supported yet!
 	}
-	tempstep = adlibenv[curchan]; //Current volume!
-	#ifdef __IGNORE_ADSR
-		tempstep = __IGNORE_ADSR; //Don't step: always full percentage!
-	#else
-		if (tempstep>1.0f) tempstep = 1.0f; //Limit volume to 100%!
-	#endif
-	tempsample = (char) (tempsample * tempstep);
 
-	if (adlibstep[curchan]>fullstep) adlibstep[curchan] = 0;
-	return (tempsample);
+	//Determine our active operators!
+	operator1 = adliboperators[0][curchan]; //First operator to use!
+	operator2 = adliboperators[1][curchan]; //Second operator to use!
+
+	//Determine the type of modulation!
+	//Operator 1!
+	effectivemodulator = (adlibfreq(curchan)*adlibop[operator1].ModulatorFrequencyMultiple); //Effective modulator frequency!
+	fullstep1 = usesamplerate / effectivemodulator;
+	tempsample = oplwave[adlibop[operator1].wavesel&wavemask][(uint8_t)((double)adlibstep[operator1]++ / ((double)fullstep1 / (double)256))];
+	if (adlibop[operator1].AM) tempsample *= AMDepth; //Apply AM depth!
+
+	//Apply the volume envelope for operator 1!
+	tempstep = adlibenv[operator1]; //Current volume!
+	if (tempstep > 1.0f) tempstep = 1.0f; //Limit volume to 100%!
+	tempsample *= tempstep;
+	tempsample *= 2.0f;
+
+	//Operator 2!
+	effectivecarrier = (adlibfreq(curchan)*adlibop[operator2].ModulatorFrequencyMultiple); //Effective carrier init!
+	if (!adlibch[curchan].synthmode) effectivecarrier *= ((float)tempsample/117.0f); //FM using the first operator when needed!
+	fullstep2 = usesamplerate / effectivecarrier;
+	result = oplwave[adlibop[operator2].wavesel&wavemask][(uint8_t)((double)adlibstep[operator2]++ / ((double)fullstep2 / (double)256))];
+	if (adlibop[operator2].AM) result *= AMDepth; //Apply AM depth!
+
+	//Apply the volume envelope for operator 2!
+	tempstep = adlibenv[operator2]; //Current volume!
+	if (tempstep > 1.0f) tempstep = 1.0f; //Limit volume to 100%!
+	result *= tempstep;
+	result *= 2.0f;
+
+	//Perform additive synthesis when asked to do so.
+	if (adlibch[curchan].synthmode) result += tempsample; //Perform additive synthesis when needed!
+
+	//Make sure we don't overflow!
+	if (adlibstep[operator1] > fullstep1) adlibstep[operator1] = 0;
+	if (adlibstep[operator2] > fullstep2) adlibstep[operator2] = 0;
+
+	return (char)(result);
 }
 
 OPTINLINE char adlibgensample() {
 	uint8_t curchan;
 	char adlibaccum;
 	adlibaccum = 0;
-	for (curchan=0; curchan<9; curchan++) {
-			if (adlibfreq (curchan) !=0) {
-					adlibaccum += adlibsample (curchan);
-				}
+	for (curchan = 0; curchan < 9; curchan++)
+	{
+		if (adlibfreq(curchan) != 0)
+		{
+			adlibaccum += adlibsample(curchan);
 		}
+	}
 	return (adlibaccum);
 }
 
 void tickadlib() {
 	uint8_t curchan;
-	for (curchan=0; curchan<9; curchan++) {
-			if (adlibfreq (curchan) !=0) {
-					if (adlibdidattack[curchan]) {
-							adlibenv[curchan] *= adlibdecay[curchan];
-						}
-					else {
-							adlibenv[curchan] *= adlibattack[curchan];
-							if (adlibenv[curchan]>=1.0) adlibdidattack[curchan] = 1;
-						}
+	for (curchan = 0; curchan<0x16; curchan++)
+	{
+		if (adlibfreq(adliboperatorsreverse[curchan]) != 0)
+		{
+			if (adlibdidattack[curchan])
+			{
+				if (adlibenv[curchan]>0.0001f)
+				{
+					adlibenv[curchan] *= adlibdecay[curchan];
 				}
+				else
+				{
+					adlibenv[curchan] = 0; //Clear: nothing to be heard!
+				}
+			}
+			else
+			{
+				adlibenv[curchan] *= adlibattack[curchan];
+				if (adlibenv[curchan] >= 1.0) adlibdidattack[curchan] = 1;
+			}
 		}
+	}
 }
-
-//sword audbuf[96000]; //Audio buffer!
-//uint_32 audbufptr = 0; //Current audiobuffer pointer!
 
 byte adlib_soundGenerator(void* buf, uint_32 length, byte stereo, void *userdata) //Generate a sample!
 {
@@ -274,6 +345,7 @@ byte adlib_soundGenerator(void* buf, uint_32 length, byte stereo, void *userdata
 	for (curchan=0; curchan<9; curchan++) { //Check for active channels!
 			if (adlibfreq (curchan) !=0) {
 				filled = 1; //We're filled!
+				break; //Stop searching!
 			}
 		}
 	
@@ -300,7 +372,10 @@ byte adlib_soundGenerator(void* buf, uint_32 length, byte stereo, void *userdata
 void initAdlib()
 {
 	if (__HW_DISABLED) return; //Abort!
+
 	//All input!
+	AMDepth = dB2factor(1.0f, __MAX_DB);
+
 	if (__SOUND_ADLIB)
 	{
 		if (!addchannel(&adlib_soundGenerator,NULL,"Adlib",usesamplerate,0,0,SMPL8S)) //Start the sound emulation (mono) with automatic samples buffer?
@@ -319,7 +394,7 @@ void initAdlib()
 	register_PORTOUT(baseport,&outadlib); //Address port (W)
 	register_PORTOUT(baseport+1,&outadlib); //Data port (W/O)
 	//dolog("adlib","Registering timer...");
-	addtimer(49716.0f,&tickadlib,"AdlibAttackDecay",ADLIBMULTIPLIER,0,NULL); //We run at 49.716Khz, about every 20us.
+	addtimer(usesamplerate,&tickadlib,"AdlibAttackDecay",ADLIBMULTIPLIER,0,NULL); //We run at 49.716Khz, about every 20us.
 	//dolog("adlib","Ready"); //Ready to run!
 }
 
