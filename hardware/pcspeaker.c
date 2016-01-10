0d5340045ea532957f546f779d5d6f2215b24800
superfury <superfury@live.nl>
2016-01-10 21:14:34 +0100

    - Removed unused pcspeaker module.

--- hardware/pcspeaker.c
+++
@@ -1,6 +0,0 @@
-#include "headers/types.h" //Basic types!
-#include "headers/emu/sound.h" //Sound!
-#include "headers/emu/emu_misc.h" //Random generators!
-#include "headers/support/highrestimer.h" //High resolution clock support!
-#include "headers/support/fifobuffer.h" //FIFO sample buffer support!
-
