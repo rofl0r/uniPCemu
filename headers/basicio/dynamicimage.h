d532bab3ea875c94a988f2ce12a3bb63cc136749
superfury <superfury@live.nl>
2016-01-25 10:48:14 +0000

    dynamicimage.h: Added dynamicimage_hassector function.

--- headers/basicio/dynamicimage.h
+++ headers/basicio/dynamicimage.h
@@ -9,4 +9,5 @@ byte dynamicimage_readsector(char *filename,uint_32 sector, void *buffer); //Rea
 FILEPOS dynamicimage_getsize(char *filename);
 
 FILEPOS generateDynamicImage(char *filename, FILEPOS size, int percentagex, int percentagey); //Generate dynamic image; returns size.
+byte dynamicimage_hassector(char *filename,uint_32 sector); //Has a 512-byte sector! Result=1 on present&filled, 0 on not present or error! Used for simply copying the sector to a different image!
 #endif
