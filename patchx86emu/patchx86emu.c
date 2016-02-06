cb955298c94c516b077703a97cd17b727ee9f07a
superfury <superfury@live.nl>
2016-02-06 06:04:24 +0100

    - Added a simple program to the project to patch the emulator to not use a console window, since the normal compiler flags won't work in the used MinGW version it's being developed on.

---
+++ patchx86emu/patchx86emu.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //I/O support!
+#include <windows.h>
+#include <commctrl.h>
+
+// Our application entry point.
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+  FILE *f;
+  unsigned long int offset;
+  WORD subsystem = 2; //The Windows GUI application subsystem!
+  f = fopen("x86EMU.exe","rb+");
+  if (!f) return 0; //Abort if not found!
+  fseek(f,0x3C,SEEK_SET); //Goto offset of our PE header location!
+  if (fread(&offset,1,4,f)!=4) //Read the offset we need!
+  {
+    fclose(f);
+    return 0; //Abort!
+  }
+  fseek(f,offset+0x5C,SEEK_SET); //Goto offset of our subsystem!
+  fwrite(&subsystem,1,2,f);
+  fclose(f); //Finished!
+  return 0; //Finished!
+}
