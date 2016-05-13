0c6990943c481e985c805fc7d560102d2f59c330
superfury <superfury@live.nl>
2016-05-13 22:43:04 +0200

    - Cleaned up biosmenu.h. - Increased maximum list items to 1000 for large filelists.

--- headers/bios/biosmenu.h
+++ headers/bios/biosmenu.h
@@ -8,11 +8,10 @@
 //Default item!
 #define FILELIST_DEFAULT -3
 
-#define ITEMLIST_MAXITEMS 0x100
+#define ITEMLIST_MAXITEMS 1000
 
 void allocBIOSMenu(); //Stuff that take extra video memory etc. for seperated BIOS allocation (so before MMU, because it may take it all)!
 void freeBIOSMenu(); //Free up all BIOS related memory!
-void initBIOSMenu(); //Stuff that take extra video memory etc. for seperated BIOS!
 
 int CheckBIOSMenu(uint_32 timeout); //To run the BIOS Menus! Result: to reboot?
 byte runBIOS(byte showloadingtext); //Run the BIOS!
