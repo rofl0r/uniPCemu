dd2431cc15890bc199f81284299fb2bf76fa23aa
superfury <superfury@live.nl>
2016-08-08 15:29:06 +0200

    - Implemented basic Sound Blaster buffers, PIC and DMA, IO and reset.

--- hardware/soundblaster.c
+++ hardware/soundblaster.c
@@ -5,15 +5,34 @@
 #include "headers/support/log.h" //Logging support!
 #include "headers/hardware/ports.h" //I/O support!
 #include "headers/hardware/adlib.h" //Adlib card has more ports with Sound Blasters!
+#include "headers/hardware/pic.h" //Interrupt support!
+#include "headers/hardware/8237A.h" //DMA support!
 
 #define __SOUNDBLASTER_SAMPLERATE 22050.0f
 #define __SOUNDBLASTER_SAMPLEBUFFERSIZE 2048
 #define SOUNDBLASTER_VOLUME 100.0f
+//Size of the input buffer of the DSP chip!
+#define __SOUNDBLASTER_DSPINDATASIZE 16
+//Big enough output buffer for all ranges available!
+#define __SOUNDBLASTER_DSPOUTDATASIZE 0x10000
+
+//IRQ/DMA assignments!
+#define __SOUNDBLASTER_IRQ8 7
+#define __SOUNDBLASTER_DMA8 1
 
 struct
 {
 	word baseaddr;
 	SOUNDDOUBLEBUFFER soundbuffer; //Outputted sound to render!
+	FIFOBUFFER *DSPindata; //Data to be read from the DSP!
+	FIFOBUFFER *DSPoutdata; //Data to be rendered for the DSP!
+	byte resetport;
+	sword command; //The current command we're processing (-1 for none)
+	byte commandstep; //The step within the command!
+	uint_32 dataleft; //The position(in bytes left) within the command during the data phase!
+	byte busy; //Are we busy (not able to receive data/commands)?
+	byte IRQ8Pending; //Is a 8-bit IRQ pending?
+	byte DREQ; //Our current DREQ signal for transferring data!
 } SOUNDBLASTER; //The Sound Blaster data!
 
 double soundblaster_soundtiming = 0.0, soundblaster_soundtick = 1000000000.0 / __SOUNDBLASTER_SAMPLERATE;
@@ -84,8 +103,39 @@ byte SoundBlaster_soundGenerator(void* buf, uint_32 length, byte stereo, void *u
 	}
 }
 
+void DSP_writeData(byte data)
+{
+}
+
+void DSP_writeCommand(byte command)
+{
+}
+
+byte DSP_readData()
+{
+	byte result;
+	readfifobuffer(SOUNDBLASTER.DSPindata, &result); //Read the result, if any!
+	return result; //Give the data!
+}
+
+void DSP_writeDataCommand(byte value)
+{
+	if (SOUNDBLASTER.busy == 0) //Ready to process?
+	{
+		if (SOUNDBLASTER.command != -1) //Handling data?
+		{
+			DSP_writeData(value); //Writing data!
+		}
+		else //Writing a command?
+		{
+			DSP_writeCommand(value); //Writing command!
+		}
+	}
+}
+
 byte inSoundBlaster(word port, byte *result)
 {
+	byte dummy;
 	if ((port&~0xF)!=SOUNDBLASTER.baseaddr) return 0; //Not our base address?
 	switch (port & 0xF) //What port?
 	{
@@ -94,15 +144,15 @@ byte inSoundBlaster(word port, byte *result)
 		return 1; //Handled!
 		break;
 	case 0xA: //DSP - Read data
-		*result = 0x00;
+		*result = DSP_readData(); //Read the result, if any!
 		return 1; //Handled!
 		break;
 	case 0xC: //DSP - Write Buffer Status
-		*result = 0x00;
+		*result = SOUNDBLASTER.busy?0x80:0x00; //Are we ready to write data? 0x80=Not ready to write.
 		return 1; //Handled!
 	case 0xE: //DSP - Data Available Status, DSP - IRQ Acknowledge, 8-bit
-		*result = 0x00;
-		return 1;
+		*result = (peekfifobuffer(SOUNDBLASTER.DSPindata,&dummy)<<7); //Do we have data available?
+		return 1; //We have a result!
 	default:
 		break;
 	}
@@ -115,6 +165,14 @@ byte outSoundBlaster(word port, byte value)
 	switch (port & 0xF) //What port?
 	{
 	case 0x6: //DSP - Reset?
+		if (value == 0 && (SOUNDBLASTER.resetport == 1)) //1 going 0? Perform reset!
+		{
+			SOUNDBLASTER.command = -1; //No command!
+			SOUNDBLASTER.busy = 0; //Not busy!
+
+			writefifobuffer(SOUNDBLASTER.DSPindata,0xAA); //We've reset!
+			fifobuffer_gotolast(SOUNDBLASTER.DSPindata); //Force the data to the user!
+		}
 		return 1; //Handled!
 		break;
 	case 0x8: //FM Music - Compatible Register port
@@ -125,6 +183,7 @@ byte outSoundBlaster(word port, byte value)
 		writeadlibdata(value); //Write to the data port!
 		return 1; //Handled!
 	case 0xC: //DSP - Write Data or Command
+		DSP_writeDataCommand(value); //Write data or command!
 		return 1; //Handled!
 	default:
 		break;
@@ -132,33 +191,83 @@ byte outSoundBlaster(word port, byte value)
 	return 0; //Not supported yet!
 }
 
+void StartPendingSoundBlasterIRQ(byte IRQ)
+{
+	SOUNDBLASTER.IRQ8Pending = 1; //We're starting to pend our IRQ!
+}
+
+byte SoundBlaster_readDMA8()
+{
+	byte result;
+	readfifobuffer(SOUNDBLASTER.DSPindata,&result); //Read anyway!
+	return result; //Give the data read!
+}
+
+void SoundBlaster_writeDMA8(byte data)
+{
+	DSP_writeDataCommand(data); //Write the Data/Command!
+}
+
+void SoundBlaster_DREQ()
+{
+	DMA_SetDREQ(__SOUNDBLASTER_DMA8,SOUNDBLASTER.DREQ); //Set the DREQ signal accordingly!
+}
+
+void SoundBlaster_DACK()
+{
+	//We're transferring something?
+	SOUNDBLASTER.busy = 1; //We're busy!
+}
+
+void SoundBlaster_TC()
+{
+	//We're finished?
+	SOUNDBLASTER.busy = 0; //We're not busy anymore!
+}
+
 void initSoundBlaster(word baseaddr)
 {
-	if (allocDoubleBufferedSound16(__SOUNDBLASTER_SAMPLEBUFFERSIZE, &SOUNDBLASTER.soundbuffer, 0)) //Valid buffer?
+	SOUNDBLASTER.baseaddr = 0; //Default: no sound blaster emulation!
+	if (SOUNDBLASTER.DSPindata = allocfifobuffer(__SOUNDBLASTER_DSPINDATASIZE,0)) //DSP read data buffer!
 	{
-		if (!addchannel(&SoundBlaster_soundGenerator, NULL, "SoundBlaster", __SOUNDBLASTER_SAMPLERATE, __SOUNDBLASTER_SAMPLEBUFFERSIZE, 0, SMPL16S)) //Start the sound emulation (mono) with automatic samples buffer?
+		if (SOUNDBLASTER.DSPoutdata = allocfifobuffer(__SOUNDBLASTER_DSPOUTDATASIZE,0)) //DSP write data buffer!
 		{
-			dolog("adlib", "Error registering sound channel for output!");
+			if (allocDoubleBufferedSound16(__SOUNDBLASTER_SAMPLEBUFFERSIZE, &SOUNDBLASTER.soundbuffer, 0)) //Valid buffer?
+			{
+				if (!addchannel(&SoundBlaster_soundGenerator, NULL, "SoundBlaster", __SOUNDBLASTER_SAMPLERATE, __SOUNDBLASTER_SAMPLEBUFFERSIZE, 0, SMPL16S)) //Start the sound emulation (mono) with automatic samples buffer?
+				{
+					dolog("adlib", "Error registering sound channel for output!");
+				}
+				else
+				{
+					setVolume(&SoundBlaster_soundGenerator, NULL, SOUNDBLASTER_VOLUME);
+					SOUNDBLASTER.baseaddr = baseaddr; //The base address to use!
+				}
+			}
+			else
+			{
+				dolog("adlib", "Error registering double buffer for output!");
+			}
 		}
-		else
-		{
-			setVolume(&SoundBlaster_soundGenerator, NULL, SOUNDBLASTER_VOLUME);
-		}
-	}
-	else
-	{
-		dolog("adlib", "Error registering double buffer for output!");
 	}
 
-	SOUNDBLASTER.baseaddr = baseaddr; //The base address to use!
+	SOUNDBLASTER.resetport = 0xFF; //Reset the reset port!
+	SOUNDBLASTER.busy = 0; //Default to not busy!
+	SOUNDBLASTER.DREQ = 0; //Not requesting anything!
+	SOUNDBLASTER.IRQ8Pending = 0; //Not pending anything!
 
 	register_PORTIN(&inSoundBlaster); //Status port (R)
 	//All output!
 	register_PORTOUT(&outSoundBlaster); //Address port (W)
+	registerIRQ(__SOUNDBLASTER_IRQ8,&StartPendingSoundBlasterIRQ,NULL); //Pending SB IRQ only!
+	registerDMA8(__SOUNDBLASTER_DMA8,&SoundBlaster_readDMA8,&SoundBlaster_writeDMA8); //DMA access of the Sound Blaster!
+	registerDMATick(__SOUNDBLASTER_DMA8,&SoundBlaster_DREQ,&SoundBlaster_DACK,&SoundBlaster_TC);
 }
 
 void doneSoundBlaster()
 {
 	removechannel(&SoundBlaster_soundGenerator, NULL, 0); //Stop the sound emulation?
 	freeDoubleBufferedSound(&SOUNDBLASTER.soundbuffer); //Free our double buffered sound!
+	free_fifobuffer(&SOUNDBLASTER.DSPindata); //Release our input buffer!
+	free_fifobuffer(&SOUNDBLASTER.DSPoutdata); //Release our output buffer!
 }
