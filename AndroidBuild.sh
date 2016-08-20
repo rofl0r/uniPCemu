91220acb14038918f122d917d4853ce869be2f3c
superfury <superfury@live.nl>
2016-08-20 18:32:45 +0200

    - Added Android Makefile support. - Improved Androidbuild shell script. - Added new icons for UniPCemu.

--- AndroidBuild.sh
+++ AndroidBuild.sh
@@ -5,5 +5,5 @@ LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 JOBS=1
 
-../setEnvironment-$1.sh sh -c "cd UniPCemu && make --makefile=Makefile PLATFORM=android -j1" && mv -f UniPCemu/UniPCemu libapplication-$1.so
+../setEnvironment-$1.sh sh -c "cd UniPCemu && make --makefile=Makefile PLATFORM=android -j1" && mv -f UniPCemu/profUniPCemu libapplication-$1.so
 
