25069520f0d4d11904463377f8c4e7b76edae1e2
superfury <superfury@live.nl>
2016-09-05 10:32:04 +0200

    - Fixed VGA Timer initialization requiring an active VGA to be set.

--- UniPCemu/emu/core/emucore.c
+++ UniPCemu/emu/core/emucore.c
@@ -326,14 +326,13 @@ void initEMU(int full) //Init!
 	debugrow("Initializing VGA...");	
 	//First, VGA allocations for seperate systems!
 
-	VGA_initTimer(); //Initialise the VGA timer for usage!
-
 	uint_32 VRAMSizeBackup;
 	VRAMSizeBackup = BIOS_Settings.VRAM_size; //Save the original VRAM size for extensions!
 
 	MainVGA = VGAalloc(0,1,(BIOS_Settings.VGA_Mode==6)?1:(BIOS_Settings.VGA_Mode==7?2:0)); //Allocate a main VGA, automatically by BIOS!
 	debugrow("Activating main VGA engine...");
 	setActiveVGA(MainVGA); //Initialise primary VGA using the BIOS settings, for the system itself!
+	VGA_initTimer(); //Initialise the VGA timer for usage!
 	initCGA_MDA(); //Add CGA/MDA support to the VGA as an extension!
 	if ((BIOS_Settings.VGA_Mode==6) || (BIOS_Settings.VGA_Mode==7)) SVGA_Setup_TsengET4K(VRAMSizeBackup); //Start the Super VGA card instead if enabled!
 
