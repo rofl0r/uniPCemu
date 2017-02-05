a6194188eb9f24de73811e949b361bab784e525c
superfury <superfury@live.nl>
2017-02-05 03:32:20 +0100

    - Added support for inline high- and low-pass filters using defines. - Inlined the Game Blaster low-pass filters to optimize it.

--- UniPCemu/headers/support/filters.h
+++ UniPCemu/headers/support/filters.h
@@ -17,6 +17,14 @@ typedef struct
 	float samplerate;
 } HIGHLOWPASSFILTER; //High or low pass filter!
 
+
+#define applySoundLowPassFilterObj(filter,currentsample) currentsample = filter.sound_last_result = filter.sound_last_result+(filter.alpha*(currentsample-filter.sound_last_result))
+
+#define applySoundHighPassFilterObj(filter,currentsample,last_resulttmp) last_resulttmp = filter.sound_last_result; \ //Load the last result to process!
+	last_resulttmp = filter.alpha * (last_resulttmp + currentsample - filter.sound_last_sample); \
+	filter.sound_last_sample = currentsample; \ //The last sample that was processed!
+	currentsample = filter.sound_last_result = last_resulttmp; //Give the new result!
+
 //Global high and low pass filters support!
 void initSoundFilter(HIGHLOWPASSFILTER *filter, byte ishighpass, float cutoff_freq, float samplerate); //Initialize the filter!
 void updateSoundFilter(HIGHLOWPASSFILTER *filter, byte ishighpass, float cutoff_freq, float samplerate); //Update the filter information/type!
--- UniPCemu/support/filters.c
+++ UniPCemu/support/filters.c
@@ -35,12 +35,12 @@ void applySoundFilter(HIGHLOWPASSFILTER *filter, float *currentsample)
 	if (filter->isHighPass) //High-pass filter?
 	{
 		last_result = filter->alpha * (last_result + *currentsample - filter->sound_last_sample);
-		filter->sound_last_sample = *currentsample; //The last sample that was processed!
 	}
 	else //Low-pass filter?
 	{
 		last_result += (filter->alpha*(*currentsample-last_result));
 	}
+	filter->sound_last_sample = *currentsample; //The last sample that was processed!
 	*currentsample = filter->sound_last_result = last_result; //Give the new result!
 }
 
@@ -53,10 +53,9 @@ void applySoundHighPassFilter(HIGHLOWPASSFILTER *filter, float *currentsample)
 	*currentsample = filter->sound_last_result = last_result; //Give the new result!
 }
 
+#define calcSoundLowPassFilter(filter,currentsample) *currentsample = filter->sound_last_result = filter->sound_last_result+(filter->alpha*((*currentsample)-filter->sound_last_result))
+
 void applySoundLowPassFilter(HIGHLOWPASSFILTER *filter, float *currentsample)
 {
-	INLINEREGISTER float last_result;
-	last_result = filter->sound_last_result; //Load the last result to process!
-	last_result += (filter->alpha*(*currentsample-last_result));
-	*currentsample = filter->sound_last_result = last_result; //Give the new result!
+	calcSoundLowPassFilter(filter,currentsample); //Filter manually!
 }
