1523d7b253886ee84adefa1cb8c5eebe1bec3c2d
superfury <superfury@live.nl>
2017-03-19 15:33:11 +0100

    - Improved Android_static.mk to make it compatible with Android.mk.

--- android-project/jni/src/Android_static.mk
+++ android-project/jni/src/Android_static.mk
@@ -4,15 +4,23 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
+SDL_PATH := ../SDL2
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include ../UniPCemu
+
 LOCAL_CFLAGS := -DSDL2
 
 ROOTPATH = ../UniPCemu
 
 include ../UniPCemu/Makefile
 
-LOCAL_SRC_FILES := $(patsubst %.o,../../../UniPCemu/%.c,$(OBJS))
+# Add your application source files here...
+LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+	$(patsubst %.o,../../../UniPCemu/%.c,$(OBJS))
 
 LOCAL_STATIC_LIBRARIES := SDL2_static
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
 include $(BUILD_SHARED_LIBRARY)
 $(call import-module,SDL)LOCAL_PATH := $(call my-dir)
