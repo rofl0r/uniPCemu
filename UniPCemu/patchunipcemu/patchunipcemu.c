9d5aef4683284650aecd7619976b6cc23f8704d7
superfury <superfury@live.nl>
2017-03-29 15:44:07 +0200

    - Fixed the UniPCemu Windows patching program to apply itself to both new x86 and x64 executables.

--- UniPCemu/patchunipcemu/patchunipcemu.c
+++ UniPCemu/patchunipcemu/patchunipcemu.c
@@ -7,7 +7,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   FILE *f;
   unsigned long int offset;
   WORD subsystem = 2; //The Windows GUI application subsystem!
-  f = fopen("UniPCemu.exe","rb+");
+  //First, 32-bit version!
+  f = fopen("UniPCemu_x86.exe","rb+");
+  if (!f) goto try64; //Abort if not found!
+  fseek(f,0x3C,SEEK_SET); //Goto offset of our PE header location!
+  if (fread(&offset,1,4,f)!=4) //Read the offset we need!
+  {
+    fclose(f);
+    goto try64; //Abort!
+  }
+  fseek(f,offset+0x5C,SEEK_SET); //Goto offset of our subsystem!
+  fwrite(&subsystem,1,2,f);
+  fclose(f); //Finished!
+
+  //Second, 64-bit version!
+  try64:
+  f = fopen("UniPCemu_x64.exe","rb+");
   if (!f) return 0; //Abort if not found!
   fseek(f,0x3C,SEEK_SET); //Goto offset of our PE header location!
   if (fread(&offset,1,4,f)!=4) //Read the offset we need!
@@ -18,5 +33,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   fseek(f,offset+0x5C,SEEK_SET); //Goto offset of our subsystem!
   fwrite(&subsystem,1,2,f);
   fclose(f); //Finished!
+
   return 0; //Finished!
 }
