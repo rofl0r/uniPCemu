5d5b1b064913180f001338c6e6c4200771d36ed6
superfury <superfury@live.nl>
2017-05-05 20:32:55 +0200

    - Modified the UniPCemu supported platforms for compiling for Android-NDK-profiler to it's supported platforms only.

--- android-project/jni/Application.mk
+++ android-project/jni/Application.mk
@@ -3,4 +3,10 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 APP_STL := stlport_static 
 
+ifneq (,$(profile))
+#Support specific platforms only for this profiler!
+APP_ABI := armeabi armeabi-v7a
+else
+#Support all platforms
 APP_ABI := all
+endif
