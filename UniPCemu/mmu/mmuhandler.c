93b88881cd7ca609b96ac485511dfe1dd0683d6e
superfury <superfury@live.nl>
2017-06-06 20:44:35 +0200

    - Added documentation about Compaq RAM mapping.

--- UniPCemu/mmu/mmuhandler.c
+++ UniPCemu/mmu/mmuhandler.c
@@ -353,6 +353,7 @@ OPTINLINE void applyMemoryHoles(uint_32 *realaddress, byte *nonexistant, byte is
 			*realaddress -= (uint_32)((uint_64)HIGH_MEMORYHOLE_END - (uint_64)HIGH_MEMORYHOLE_START); //Patch into memory hole!
 		}
 	}
+	//TODO(According to PCJs): Compaq has 384Kb of RAM at 0xFA0000-0xFFFFFF always. The rest of RAM is mapped low and above 16MB. The FE0000-FFFFFF range can be remapped to E0000-FFFFF, while it can be write-protected.
 	if ((originaladdress>=0xFE0000) && (originaladdress<=0xFFFFFF)) //Special area addressed?
 	{
 		if (memoryprotect_FE0000 && iswrite) //Memory protected?
