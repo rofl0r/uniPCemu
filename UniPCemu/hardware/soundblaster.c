190a3052c30c042fc181a03f20d1ae5d8d4dca7e
superfury <superfury@live.nl>
2017-06-27 11:54:16 +0200

    - Enabled Sound Blaster test wave recording for testing.

--- UniPCemu/hardware/soundblaster.c
+++ UniPCemu/hardware/soundblaster.c
@@ -39,9 +39,9 @@
 #define MIN_ADAPTIVE_STEP_SIZE 0
 
 //Record a test wave!
-//#define RECORD_TESTWAVE
+#define RECORD_TESTWAVE
 //Log what we record?
-#define LOG_RECORDING
+//#define LOG_RECORDING
 
 //Enable below define to log all command bytes sent.
 //#define SOUNDBLASTER_LOG
