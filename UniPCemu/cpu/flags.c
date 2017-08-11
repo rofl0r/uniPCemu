be8e2a7c3194d23e9a7af8c48eea3d823f858ba2
superfury <superfury@live.nl>
2017-08-11 14:18:39 +0200

    - Generalized add/sub/adc/sbb C/O/A flags calculations with standard handlers called by each of them.

--- UniPCemu/cpu/flags.c
+++ UniPCemu/cpu/flags.c
@@ -13,6 +13,8 @@ byte parity[0x100] = { //All parity values!
 #include "headers/cpu/cpu.h" //CPU!
 #include "headers/cpu/easyregs.h" //EASY Regs!
 
+//Sign and parity logic
+
 void flag_p8(uint8_t value)
 {
 	FLAGW_PF(parity[value]);	
@@ -46,6 +48,8 @@ void flag_s32(uint32_t value)
 	else FLAGW_SF(0);
 }
 
+//Sign, Zero and Parity logic
+
 void flag_szp8(uint8_t value)
 {
 	if (!value) FLAGW_ZF(1);
@@ -73,6 +77,8 @@ void flag_szp32(uint32_t value)
 	FLAGW_PF(parity[value & 255]);
 }
 
+//Logarithmic logic
+
 void flag_log8(uint8_t value)
 {
 	flag_szp8(value);
@@ -94,155 +100,170 @@ void flag_log32(uint32_t value)
 	FLAGW_OF(0);
 }
 
+//Negate a variable!
+#define NEG(x) ((~(x))+1)
+
+//Addition Carry, Overflow, Adjust logic
+//Tables based on http://teaching.idallen.com/dat2343/10f/notes/040_overflow.txt
+//Index Bit0=sumsign, Bit1=num2sign(add or sub negated value), Bit2=num1sign(v1)
+byte addoverflow[8] = {0,1,0,0,0,0,1,0};
+byte suboverflow[8] = {0,0,0,1,1,0,0,0};
+
+//Flags(Aux/Carry):
+//ADD: ((op1)^(op2))^(((op1)^(result))&~((op1)^(op2)))^result
+//SUB: ((op1)^(op2))^(((op1)^(result))&((op1)^(op2)))^result
+//Auxiliary flag: ((op1^op2)^((op1^result)&(~(op1^op2))^result
+//Auxiliary flag: ((op1)^(op2))^(((op1)^(result))&~((op1)^(op2)))^result
+//borrow/carry bits
+#define bcbitsa(v1,v2) (((v1)^(v2))^(((v1)^(dst))&~((v1)^(v2)))^dst)
+#define bcbitss(v1,v2) (((v1)^(v2))^(((v1)^(dst))&((v1)^(v2)))^dst)
+
+void flag_adcoa8(uint8_t v1, uint16_t add, uint16_t dst)
+{
+	FLAGW_CF((bcbitsa((uint16_t)v1,add)>>7)&1); //Carry?
+	FLAGW_OF(addoverflow[((dst>>7)&1)|(((add>>6)&2))|((v1>>5)&4)]); //Overflow?
+	FLAGW_AF((bcbitsa((uint16_t)v1,add)&0x8)>>3); //Adjust?
+}
+
+void flag_adcoa16(uint16_t v1, uint32_t add, uint32_t dst)
+{
+	FLAGW_CF((bcbitsa((uint32_t)v1,add)>>15)&1); //Carry?
+	FLAGW_OF(addoverflow[((dst>>15)&1)|(((add>>14)&2))|((v1>>13)&4)]); //Overflow?
+	FLAGW_AF((bcbitsa((uint32_t)v1,add)&0x8)>>3); //Adjust?
+}
+
+void flag_adcoa32(uint32_t v1, uint64_t add, uint64_t dst)
+{
+	FLAGW_CF((bcbitsa((uint64_t)v1,add)>>31)&1); //Carry?
+	FLAGW_OF(addoverflow[((dst>>31)&1)|(((add>>30)&2))|((v1>>29)&4)]); //Overflow?
+	FLAGW_AF((bcbitsa((uint32_t)v1,add)&0x8)>>3); //Adjust?
+}
+
+//Substract Carry, Overflow, Adjust logic
+void flag_subcoa8(uint8_t v1, uint16_t sub, uint16_t dst)
+{
+	FLAGW_CF((bcbitss((uint16_t)v1,sub)>>7)&1); //Carry?
+	FLAGW_OF(suboverflow[((dst>>7)&1)|((sub>>6)&2)|((v1>>5)&4)]); //Overflow?
+	FLAGW_AF((bcbitss((uint16_t)v1,sub)&0x8)>>3); //Adjust?
+}
+
+void flag_subcoa16(uint16_t v1, uint32_t sub, uint32_t dst)
+{
+	FLAGW_CF((bcbitss((uint32_t)v1,sub)>>15)&1); //Carry?
+	FLAGW_OF(suboverflow[((dst>>15)&1)|((sub>>14)&2)|((v1>>13)&4)]); //Overflow?
+	FLAGW_AF((bcbitss((uint32_t)v1,sub)&0x8)>>3); //Adjust?
+}
+
+void flag_subcoa32(uint32_t v1, uint64_t sub, uint64_t dst)
+{
+	FLAGW_CF((bcbitss((uint64_t)v1,sub)>>31)&1); //Carry?
+	FLAGW_OF(suboverflow[((dst>>31)&1)|((sub>>30)&2)|((v1>>29)&4)]); //Overflow?
+	FLAGW_AF((bcbitss((uint64_t)v1,sub)&0x8)>>3); //Adjust?
+}
+
+//Start of the externally used calls to calculate flags!
+
 void flag_adc8(uint8_t v1, uint8_t v2, uint8_t v3)
 {
-	int16_t dst,add=(int16_t)v2 + (int16_t)v3;
-	dst = (int16_t)v1 + add;
+	uint16_t dst;
+	uint16_t add=(uint16_t)v2 + (uint16_t)v3;
+	dst = (uint16_t)v1 + add;
 	flag_szp8((uint8_t)(dst&0xFF));
-	if (((dst ^ v1) & (dst ^ add) & 0x80) == 0x80) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	FLAGW_CF((((v1^add^dst) & 0x100)>>8)); //Set the carry flag accordingly!
-	if (((v1 ^ add ^ dst) & 0x10) == 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_adcoa8(v1,add,dst);
 }
 
 void flag_adc16(uint16_t v1, uint16_t v2, uint16_t v3)
 {
-	int32_t dst,add=(int32_t)v2 + (int32_t)v3;
-	dst = (int32_t)v1 + add;
+	uint32_t dst;
+	uint32_t add=(uint32_t)v2 + (uint32_t)v3;
+	dst = (uint32_t)v1 + add;
 	flag_szp16(dst);
-	if (((dst ^ v1) & (dst ^ add) & 0x8000) == 0x8000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	FLAGW_CF((((v1^add^dst) & 0x10000)>>16)); //Set the carry flag accordingly!
-	if (((v1 ^ add ^ dst) & 0x10) == 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_adcoa16(v1,add,dst);
 }
 
 void flag_adc32(uint32_t v1, uint32_t v2, uint32_t v3)
 {
-	int64_t dst, add=(int64_t)v2 + (int64_t)v3;
-	dst = (int64_t)v1 + add;
+	uint64_t dst;
+	uint64_t add=(uint64_t)v2 + (uint64_t)v3;
+	dst = (uint64_t)v1 + add;
 	flag_szp32((uint32_t)dst);
-	if (((dst ^ v1) & (dst ^ add) & 0x80000000) == 0x80000000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	FLAGW_CF((((v1^add^dst) & 0x100000000ULL)>>32)); //Set the carry flag accordingly!
-	if (((v1 ^ add ^ dst) & 0x10) == 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_adcoa32(v1,add,dst);
 }
 
 void flag_add8(uint8_t v1, uint8_t v2)
 {
-	int16_t dst;
-	dst = (int16_t)v1 + (int16_t)v2;
+	uint16_t dst;
+	dst = (uint16_t)v1 + (uint16_t)v2;
 	flag_szp8((uint8_t)(dst&0xFF));
-	if ((v1^v2^dst) & 0x100) FLAGW_CF(1);
-	else FLAGW_CF(0);
-	if (((dst ^ v1) & (dst ^ v2) & 0x80) == 0x80) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if (((v1 ^ v2 ^ dst) & 0x10) == 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_adcoa8(v1,v2,dst);
 }
 
 void flag_add16(uint16_t v1, uint16_t v2)
 {
-	int32_t dst;
-	dst = (int32_t)v1 + (int32_t)v2;
+	uint32_t dst;
+	dst = (uint32_t)v1 + (uint32_t)v2;
 	flag_szp16(dst);
-	if ((v1^v2^dst) & 0x10000) FLAGW_CF(1);
-	else FLAGW_CF(0);
-	if (((dst ^ v1) & (dst ^ v2) & 0x8000) == 0x8000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if (((v1 ^ v2 ^ dst) & 0x10) == 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_adcoa16(v1,v2,dst);
 }
 
 void flag_add32(uint32_t v1, uint32_t v2)
 {
-	int64_t dst;
-	dst = (int64_t)v1 + (int64_t)v2;
+	uint64_t dst;
+	dst = (uint64_t)v1 + (uint64_t)v2;
 	flag_szp32((uint32_t)dst);
-	if ((v1^v2^dst) & 0x100000000ULL) FLAGW_CF(1);
-	else FLAGW_CF(0);
-	if (((dst ^ v1) & (dst ^ v2) & 0x80000000) == 0x80000000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if (((v1 ^ v2 ^ dst) & 0x10) == 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_adcoa32(v1,v2,dst);
 }
 
 void flag_sbb8(uint8_t v1, uint8_t v2, uint8_t v3)
 {
-	int16_t dst,sub;
-	sub = v2;
-	sub += v3;
-	dst = (int16_t)v1 - (int16_t)sub;
+	uint16_t dst,sub;
+	sub = (uint16_t)v2+(uint16_t)v3;
+	dst = (uint16_t)v1 - sub;
 	flag_szp8(dst & 0xFF);
-	FLAGW_CF((((v1^sub^dst) & 0x100)>>8));
-	if ((dst ^ v1) & (v1 ^ sub) & 0x80) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if ((v1 ^ sub ^ dst) & 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_subcoa8(v1,sub,dst);
 }
 
 void flag_sbb16(uint16_t v1, uint16_t v2, uint16_t v3)
 {
-	int32_t dst,sub;
-	sub = v2;
-	sub += v3;
-	dst = (int32_t)v1 - (int32_t)sub;
+	uint32_t dst,sub;
+	sub = (uint32_t)v2+(uint32_t)v3;
+	dst = (uint32_t)v1 - sub;
 	flag_szp16(dst & 0xFFFF);
-	FLAGW_CF((((v1^sub^dst) & 0x10000)>>16));
-	if ((dst ^ v1) & (v1 ^ sub) & 0x8000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if ((v1 ^ sub ^ dst) & 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_subcoa16(v1,sub,dst);
 }
 
 void flag_sbb32(uint32_t v1, uint32_t v2, uint32_t v3)
 {
-	int64_t dst, sub;
-	sub = v2;
-	sub += v3;
-	dst = (int64_t)v1 - (int64_t)sub;
+	uint64_t dst, sub;
+	sub = (uint64_t)v2+(uint64_t)v3;
+	dst = (uint64_t)v1 - sub;
 	flag_szp32(dst & 0xFFFFFFFF);
-	FLAGW_CF((((v1^sub^dst) & 0x100000000ULL)>>32));
-	if ((dst ^ v1) & (v1 ^ sub) & 0x80000000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if ((v1 ^ sub ^ dst) & 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_subcoa32(v1,sub,dst);
 }
 
 void flag_sub8(uint8_t v1, uint8_t v2)
 {
-	int16_t dst;
-	dst = (int16_t)v1 - (int16_t)v2;
-	flag_szp8(dst & 0xFF);
-	FLAGW_CF((((v1^v2^dst) & 0x100)>>8)); //Carry?
-	if ((dst ^ v1) & (v1 ^ v2) & 0x80) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if ((v1 ^ v2 ^ dst) & 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	uint16_t dst,sub;
+	sub = (uint16_t)v2;
+	dst = (uint16_t)v1 - sub;
+	flag_szp8(dst&0xFF);
+	flag_subcoa8(v1,sub,dst);
 }
 
 void flag_sub16(uint16_t v1, uint16_t v2)
 {
-	int32_t dst;
-	dst = (int32_t)v1 - (int32_t)v2;
+	uint32_t dst,sub;
+	sub = (uint32_t)v2;
+	dst = (uint32_t)v1 - sub;
 	flag_szp16(dst & 0xFFFF);
-	FLAGW_CF((((v1^v2^dst) & 0x10000)>>16)); //Carry?
-	if ((dst ^ v1) & (v1 ^ v2) & 0x8000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if ((v1 ^ v2 ^ dst) & 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_subcoa16(v1,sub,dst);
 }
 
 void flag_sub32(uint32_t v1, uint32_t v2)
 {
-	int64_t dst;
-	dst = (int64_t)v1 - (int64_t)v2;
+	uint64_t dst,sub;
+	sub = (uint64_t)v2;
+	dst = (uint64_t)v1 - sub;
 	flag_szp32(dst & 0xFFFFFFFF);
-	FLAGW_CF((((v1^v2^dst) & 0x100000000ULL)>>32)); //Carry?
-	if ((dst ^ v1) & (v1 ^ v2) & 0x80000000) FLAGW_OF(1);
-	else FLAGW_OF(0);
-	if ((v1 ^ v2 ^ dst) & 0x10) FLAGW_AF(1);
-	else FLAGW_AF(0);
+	flag_subcoa32(v1,sub,dst);
 }
