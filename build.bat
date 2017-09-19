d52987d8c3e2c43ff93bb30f4b8bed3c96827289
superfury <superfury@live.nl>
2017-09-19 13:44:07 +0200

    - Modified build.bat syntax to include seperate install and replace the old install option with apk instead(as it builds an APK file).

--- build.bat
+++ build.bat
@@ -2,15 +2,20 @@
 if NOT "%1"=="/?" goto exec
 rem Help
 echo Android profile usage:
-echo build [install [debug]]
+echo build [apk [debug] [install]]
 echo Execute this from the android-project folder itself. Builds with default settings and SDL2_net, if present in the jni\SDL2_net folder.
+echo apk specifies to build an APK file.
+echo debug specifies the APK to use a debug configuration instead of release.
+echo install specifies the APK to be installed to the default emulator/device.
 goto finishup
 :exec
-if "%1"=="install" goto doinstall
+if "%1"=="apk" goto doapk
 if exist jni\SDL2_net\SDL_net.h ndk-build NDK_DEBUG=0 useSDL2_net=1
 if not exist jni\SDL2_net\SDL_net.h ndk-build NDK_DEBUG=0
 goto finishup
-:doinstall
-if "%2"=="debug" ant debug
-if not "%2"=="debug" ant release
+:doapk
+if "%2"=="debug" if "%3"=="" ant debug
+if "%2"=="debug" if "%3"=="install" ant debug install
+if not "%2"=="debug" if "%2"=="" ant release
+if not "%2"=="debug" if "%2"=="install" ant release install
 :finishup
