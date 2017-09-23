41f7d42c91fe8fa3fb5806edae73eb0a6a7987f4
superfury <superfury@live.nl>
2017-09-23 21:02:05 +0200

    - Fixed the patch program Makefile to use the correct filename. - Implemented support for the new program subdirectories to the patch program.

--- UniPCemu/patchunipcemu/patchunipcemu.c
+++ UniPCemu/patchunipcemu/patchunipcemu.c
@@ -1,38 +1,43 @@
 #include <stdio.h> //I/O support!
 #include <windows.h>
 
+int patchexecutable(char *filename)
+{
+	FILE *f;
+	unsigned long int offset;
+	WORD subsystem = 2; //The Windows GUI application subsystem!
+	f = fopen(filename,"rb+");
+	if (!f) return 0; //Abort if not found!
+	fseek(f,0x3C,SEEK_SET); //Goto offset of our PE header location!
+	if (fread(&offset,1,4,f)!=4) //Read the offset we need!
+	{
+		fclose(f);
+		return 1; //Abort error!
+	}
+	fseek(f,offset+0x5C,SEEK_SET); //Goto offset of our subsystem!
+	fwrite(&subsystem,1,2,f);
+	fclose(f); //Finished!
+	return 0; //OK!
+}
+
 // Our application entry point.
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-  FILE *f;
-  unsigned long int offset;
-  WORD subsystem = 2; //The Windows GUI application subsystem!
-  //First, 32-bit version!
-  f = fopen("UniPCemu_x86.exe","rb+");
-  if (!f) goto try64; //Abort if not found!
-  fseek(f,0x3C,SEEK_SET); //Goto offset of our PE header location!
-  if (fread(&offset,1,4,f)!=4) //Read the offset we need!
-  {
-    fclose(f);
-    goto try64; //Abort!
-  }
-  fseek(f,offset+0x5C,SEEK_SET); //Goto offset of our subsystem!
-  fwrite(&subsystem,1,2,f);
-  fclose(f); //Finished!
+	//First, current directory!
+	//First, 32-bit version!
+	if (patchexecutable("UniPCemu_x86.exe")) return 1; //Error!
+	//Second, 64-bit version!
+	if (patchexecutable("UniPCemu_x64.exe")) return 1; //Error!
+	//mingw subdirectories!
+	//First, 32-bit version!
+	if (patchexecutable("mingw32/UniPCemu_x86.exe")) return 1; //Error!
+	//Second, 64-bit version!
+	if (patchexecutable("mingw64/UniPCemu_x64.exe")) return 1; //Error!
+	//msys subdirectories!
+	//First, 32-bit version!
+	if (patchexecutable("msys32/UniPCemu_x86.exe")) return 1; //Error!
+	//Second, 64-bit version!
+	if (patchexecutable("msys64/UniPCemu_x64.exe")) return 1; //Error!
 
-  //Second, 64-bit version!
-  try64:
-  f = fopen("UniPCemu_x64.exe","rb+");
-  if (!f) return 0; //Abort if not found!
-  fseek(f,0x3C,SEEK_SET); //Goto offset of our PE header location!
-  if (fread(&offset,1,4,f)!=4) //Read the offset we need!
-  {
-    fclose(f);
-    return 0; //Abort!
-  }
-  fseek(f,offset+0x5C,SEEK_SET); //Goto offset of our subsystem!
-  fwrite(&subsystem,1,2,f);
-  fclose(f); //Finished!
-
-  return 0; //Finished!
+	return 0; //Finished!
 }
