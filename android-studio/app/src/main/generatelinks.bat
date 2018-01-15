27cf46d9234863d2379d64b75e2618caeba1e434
superfury <superfury@live.nl>
2018-01-15 12:06:32 +0100

    - Updated Android Studio project with a new and improved project and a new ignore file.

--- android-studio/app/src/main/generatelinks.bat
+++ android-studio/app/src/main/generatelinks.bat
@@ -1,3 +1,4 @@
 mklink /D java "..\..\..\..\android-project\src"
+mklink /D jni "..\..\..\..\android-project\jni"
 mklink /D res "..\..\..\..\android-project\res"
 mklink AndroidManifest.xml "..\..\..\..\android-project\AndroidManifest.xml"
