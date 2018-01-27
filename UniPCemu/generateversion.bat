9b9739ec1dfb5bf27f40f5ed2d32c5d2bc30d8b0
superfury <superfury@live.nl>
2018-01-27 16:20:28 +0100

    - Fixed generateversion.bat.

--- UniPCemu/generateversion.bat
+++ UniPCemu/generateversion.bat
@@ -9,4 +9,4 @@ rem copy /b gitversion1.txt + gitversion2.txt + gitversion3.txt gitcommitversion
 rem findstr /R /C:. gitcommitversiontest.tmp>gitcommitversion.h
 if NOT "%whatversion%"=="" echo %whatversion%>gitcommitversion.h
 rem Make sure the file isn't committed to the repository anymore!
-if NOT "%whatversion%"=="" git update-index --assume-unchanged 
+if NOT "%whatversion%"=="" git update-index --assume-unchanged gitcommitversion.h
