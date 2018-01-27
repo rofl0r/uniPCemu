ec8ff8c7edd8e051b3ebb7c6f5a3d0da1ebce348
superfury <superfury@live.nl>
2018-01-27 20:29:43 +0100

    - Fixed bugs in the git version batch file.

--- UniPCemu/generateversion.bat
+++ UniPCemu/generateversion.bat
@@ -4,9 +4,7 @@ set /p version1=<gitversion1.txt
 set /p version2=<gitversion2.txt
 set /p version3=<gitversion3.txt
 set whatversion=%version1%%version2%%version3%
-echo %version%
-rem copy /b gitversion1.txt + gitversion2.txt + gitversion3.txt gitcommitversiontest.tmp
-rem findstr /R /C:. gitcommitversiontest.tmp>gitcommitversion.h
 if NOT "%whatversion%"=="" echo %whatversion%>gitcommitversion.h
 rem Make sure the file isn't committed to the repository anymore!
 if NOT "%whatversion%"=="" git update-index --assume-unchanged gitcommitversion.h
+if NOT "%whatversion%"=="" echo Version information is updated!
