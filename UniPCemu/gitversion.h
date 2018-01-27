85419199c9c7f38e0164a540aac49aa386a262ef
superfury <superfury@live.nl>
2018-01-27 15:29:30 +0100

    - Added Visual Studio and MinGW Windows version information for the executable. - Added a batch file(generateversion.bat) for generating the Windows version information from the current Git commit.

---
+++ UniPCemu/gitversion.h
@@ -0,0 +1,11 @@
+#ifndef CURVERSION_H
+#define CURVERSION_H
+#include "gitcommitversion.h"
+#ifndef GITVERSION
+//Apply default version for easy compatibility!
+#define GITVERSION "1.0"
+#endif
+#define ORIGINALFILENAME "UniPCemu.exe"
+#define PRODUCTNAME "UniPCemu"
+#define COMPANYNAME "Superfury"
+#endif //CURVERSION_H
---
+++ UniPCemu/version.h
@@ -0,0 +1,24 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+//Include the current git createn version output!
+#include "gitversion.h"
+
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0\0"
+
+#define VER_PRODUCTVERSION          1,0,0,0
+#define VER_PRODUCTVERSION_STR      "1.0\0"
+
+#define VER_COMPANYNAME_STR         COMPANYNAME
+#define VER_FILEDESCRIPTION_STR     GITVERSION
+#define VER_INTERNALNAME_STR        PRODUCTNAME
+#define VER_LEGALCOPYRIGHT_STR      ""
+#define VER_LEGALTRADEMARKS1_STR    ""
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    ORIGINALFILENAME
+#define VER_PRODUCTNAME_STR         PRODUCTNAME
+
+#define VER_COMPANYDOMAIN_STR       ""
+
+#endif // VERSION_H
