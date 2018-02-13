0299609c9eb7e425e13036c5fd38bc1bc6e0cf51
superfury <superfury@live.nl>
2018-02-13 15:32:57 +0100

    - Fixed x86 real-mode IRET to work properly.

--- UniPCemu/cpu/cpu_interrupts.c
+++ UniPCemu/cpu/cpu_interrupts.c
@@ -147,12 +147,11 @@ void CPU_IRET()
 	uint_32 tempEFLAGS;
 	if (getcpumode()==CPU_MODE_REAL) //Use IVT?
 	{
-		tempSS = REG_SS;
 		//uint_32 backupESP = REG_ESP;
 		if (CPU[activeCPU].stackchecked==0) { if (checkStackAccess(3,0,0)) { return; } ++CPU[activeCPU].stackchecked; } //3 Word POPs!
 		if (CPU8086_internal_POPw(0,&IRET_IP,0)) return; //POP IP!
-		if (CPU8086_internal_POPw(0,&IRET_CS,0)) return; //POP CS!
-		if (CPU8086_internal_POPw(0,&IRET_FLAGS,0)) return; //POP FLAGS!
+		if (CPU8086_internal_POPw(2,&IRET_CS,0)) return; //POP CS!
+		if (CPU8086_internal_POPw(4,&IRET_FLAGS,0)) return; //POP FLAGS!
 		destEIP = (uint_32)IRET_IP; //POP IP!
 		segmentWritten(CPU_SEGMENT_CS,IRET_CS,3); //We're loading because of an IRET!
 		CPU_flushPIQ(-1); //We're jumping to another address!
