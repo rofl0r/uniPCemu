247ef052af8b6dc2f2082ea71abeec80fc94fa25
superfury <superfury@live.nl>
2018-02-27 10:32:52 +0100

    - Fixed XLAT 16-bit address wrapping.

--- UniPCemu/cpu/opcodes_386.c
+++ UniPCemu/cpu/opcodes_386.c
@@ -2314,13 +2314,13 @@ OPTINLINE byte CPU80386_internal_XLAT()
 	}
 	if (unlikely(CPU[activeCPU].internalinstructionstep==0)) //First step?
 	{
-		if (checkMMUaccess(CPU_segment_index(CPU_SEGMENT_DS),CPU_segment(CPU_SEGMENT_DS),(CPU_Address_size[activeCPU]?REG_EBX:REG_BX)+REG_AL,0,getCPL(),!CPU_Address_size[activeCPU],0)) return 1; //Abort on fault!
+		if (checkMMUaccess(CPU_segment_index(CPU_SEGMENT_DS),CPU_segment(CPU_SEGMENT_DS),(((CPU_Address_size[activeCPU]?REG_EBX:REG_BX)+REG_AL)&(CPU_Address_size[activeCPU]?~0:0xFFFF)),0,getCPL(),!CPU_Address_size[activeCPU],0)) return 1; //Abort on fault!
 		++CPU[activeCPU].internalinstructionstep; //Next step!
 	}
 	if (CPU[activeCPU].internalinstructionstep==1) //First Execution step?
 	{
 		//Needs a read from memory?
-		if (CPU8086_internal_stepreaddirectb(0,CPU_segment_index(CPU_SEGMENT_DS),CPU_segment(CPU_SEGMENT_DS),(CPU_Address_size[activeCPU]?REG_EBX:REG_BX)+REG_AL,&XLAT_value,!CPU_Address_size[activeCPU])) return 1; //Try to read the data!
+		if (CPU8086_internal_stepreaddirectb(0,CPU_segment_index(CPU_SEGMENT_DS),CPU_segment(CPU_SEGMENT_DS),(((CPU_Address_size[activeCPU]?REG_EBX:REG_BX)+REG_AL)&(CPU_Address_size[activeCPU]?~0:0xFFFF)),&XLAT_value,!CPU_Address_size[activeCPU])) return 1; //Try to read the data!
 		++CPU[activeCPU].internalinstructionstep; //Next internal instruction step!
 	}
 	CPUPROT1
