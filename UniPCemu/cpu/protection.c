ccc0d1dc4dee47755c31ab46a9a072aa2ca59f26
superfury <superfury@live.nl>
2018-04-23 20:13:11 +0200

    - Improved lowering CPL during interrupt gates and far return.

--- UniPCemu/cpu/protection.c
+++ UniPCemu/cpu/protection.c
@@ -949,11 +949,15 @@ SEGMENT_DESCRIPTOR *getsegment_seg(int segment, SEGMENT_DESCRIPTOR *dest, word *
 
 	if (segment==CPU_SEGMENT_CS) //We need to reload a new CPL?
 	{
-		if ((is_gated==0) && ((isJMPorCALL==3) || (isJMPorCALL==4))) //Non-gates doesn't change RPL(CPL) of CS! IRET&RETF does change CPL!
+		//Non-gates doesn't change RPL(CPL) of CS! (IRET?&)RETF does change CPL here(already done beforehand)!
+		if ((is_gated==0) && (isJMPorCALL==4)) //RETF changes privilege level?
 		{
-			CPU[activeCPU].CPL = MAX(getRPL(*segmentval),getCPL()); //Lower CPL!
-			setRPL(*segmentval,CPU[activeCPU].CPL); //Only gated loads(CALL gates) can change RPL(active lowest CPL in CS). Otherwise, it keeps the old RPL.
-			setRPL(originalval,CPU[activeCPU].CPL); //Only gated loads(CALL gates) can change RPL(active lowest CPL in CS). Otherwise, it keeps the old RPL.
+			if (getRPL(*segmentval)>getCPL()) //Lowered?
+			{
+				CPU[activeCPU].CPL = getRPL(*segmentval); //New CPL, lowered!
+				setRPL(*segmentval,CPU[activeCPU].CPL); //Only gated loads(CALL gates) can change RPL(active lowest CPL in CS). Otherwise, it keeps the old RPL.
+				setRPL(originalval,CPU[activeCPU].CPL); //Only gated loads(CALL gates) can change RPL(active lowest CPL in CS). Otherwise, it keeps the old RPL.
+			}
 		}
 	}
 
