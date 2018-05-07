0c949c4c6c29a2b1398c214db8af84227dd48f35
superfury <superfury@live.nl>
2018-05-07 18:20:57 +0200

    - Task switches caused by a normal loading instruction loading a task doesn't push an error code!

--- UniPCemu/cpu/protection.c
+++ UniPCemu/cpu/protection.c
@@ -789,7 +789,7 @@ SEGMENT_DESCRIPTOR *getsegment_seg(int segment, SEGMENT_DESCRIPTOR *dest, word *
 	if ((segment==CPU_SEGMENT_CS) && is_TSS) //Special stuff on CS, CPL, Task switch.
 	{
 		//Execute a normal task switch!
-		if (CPU_executionphase_starttaskswitch(segment,&LOADEDDESCRIPTOR,segmentval,*segmentval,isJMPorCALL,is_gated,0)) //Switching to a certain task?
+		if (CPU_executionphase_starttaskswitch(segment,&LOADEDDESCRIPTOR,segmentval,*segmentval,isJMPorCALL,is_gated,-1)) //Switching to a certain task?
 		{
 			return NULL; //Error changing priviledges or anything else!
 		}
