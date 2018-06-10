db78705c8404c4d6fcc301c08e3408d0d254f29e
superfury <superfury@live.nl>
2018-06-10 16:40:05 +0200

    - Make sure the CMOS is saved/updated when opening the Settings menu. This also prevents the changes CMOS settings from being discarded when the Settings menu changes are discarded.

--- UniPCemu/bios/biosmenu.c
+++ UniPCemu/bios/biosmenu.c
@@ -445,6 +445,8 @@ byte runBIOS(byte showloadingtext) //Run the BIOS menu (whether in emulation or 
 
 	memset(&menuoptions,0,sizeof(menuoptions)); //Init all options that might be used!
 
+	saveCMOS(); //Make sure the CMOS is properly saved when discarding any changes in the Settings menu.
+
 	BIOS_LoadData(); //Now load/reset the BIOS
 	BIOS_Changed = 0; //Default: the BIOS hasn't been changed!
 	BIOS_SaveStat = 0; //Default: not saving!
