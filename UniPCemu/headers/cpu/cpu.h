e781714ad84e7ec8aff245b1db2325e30c730b54
superfury <superfury@live.nl>
2018-08-04 13:53:40 +0200

    - Disabled the CPU timings being packed in memory, to speed things up, as it's not required to be packed.

--- UniPCemu/headers/cpu/cpu.h
+++ UniPCemu/headers/cpu/cpu.h
@@ -82,8 +82,7 @@ extern BIOS_Settings_TYPE BIOS_Settings; //BIOS Settings (required for determini
 #define u8 byte
 #define u32 uint_32
 
-#include "headers/packed.h" //Packed type!
-typedef struct PACKED
+typedef struct
 {
 	byte used; //Valid instruction? If zero, passthrough to earlier CPU timings.
 	byte has_modrm; //Do we have ModR/M parameters?
@@ -94,7 +93,6 @@ typedef struct PACKED
 	byte parameters; //The type of parameters to follow the ModR/M! 0=No parameters, 1=imm8, 2=imm16, 3=imm32, bit 2=Immediate is enabled on the REG of the RM byte(only when <2).
 	word readwritebackinformation; //The type of writing back/reading data to memory if needed! Bits 0-1: 0=None, 1=Read, Write back operation, 2=Write operation only, 3=Read operation only, Bit 4: Operates on AL/AX/EAX when set. Bit 5: Push operation. Bit 6: Pop operation. Bit 7: Allows LOCK operation(with ModR/M when memory only). Bits 8-9: Allow LOCK operation only on: 0=Memory only(default behaviour), 1=(Reg!=7 with memory only), 2=(Reg=2 or 3 with memory only), 3=(Reg=0 or 1), Bit 10: Allow LOCK operation for reg=5-7 memory only.
 } CPU_Timings; //The CPU timing information!
-#include "headers/endpacked.h" //End of packed type!
 
 #include "headers/packed.h" //Packed type!
 typedef union PACKED
