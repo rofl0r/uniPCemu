3e6122ca17a2471be88c66b0241d5e00b43c0e60
superfury <superfury@live.nl>
2018-08-18 10:20:46 +0200

    - Fixed the T-bit to check the T-bit instead of bit 0 of the Backlink.

--- UniPCemu/cpu/multitasking.c
+++ UniPCemu/cpu/multitasking.c
@@ -802,7 +802,7 @@ byte CPU_switchtask(int whatsegment, SEGMENT_DESCRIPTOR *LOADEDDESCRIPTOR,word *
 
 	if (TSSSize) //32-bit TSS?
 	{
-		if ((MMU_rw(CPU_SEGMENT_TR, CPU[activeCPU].registers->TR, 0, 1, 0) & 1) && (FLAG_RF == 0)) //Trace bit set? Cause a debug exception when this context is run?
+		if ((TSS32.T & 1) && (FLAG_RF == 0)) //Trace bit set? Cause a debug exception when this context is run?
 		{
 			if (protectedModeDebugger_taskswitched()) return 0; //Finished task switch with debugger interrupt handling!
 		}
