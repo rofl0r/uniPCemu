575d34eab6c74fe6342e0f1e041ac46db0a3bca8
superfury <superfury@live.nl>
2018-11-06 17:07:58 +0100

    - Fixed Sound Blaster IRQ being parallel correctly.

--- UniPCemu/hardware/soundblaster.c
+++ UniPCemu/hardware/soundblaster.c
@@ -22,7 +22,7 @@
 
 //IRQ/DMA assignments! Use secondary IRQ8(to prevent collisions with existing hardware!) Bochs says IRQ5? Dosbox says IRQ7?
 //8-bit IRQ&DMA!
-#define __SOUNDBLASTER_IRQ8 0x15
+#define __SOUNDBLASTER_IRQ8 0x35
 #define __SOUNDBLASTER_DMA8 1
 
 #define ADPCM_FORMAT_NONE 0x00
