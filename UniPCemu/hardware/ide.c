bacb900454817e64ac35b43b1177ced2557a20c5
superfury <superfury@live.nl>
2019-06-04 01:14:43 +0200

    - Improved ATAPI special ROM STATUS register when working as a ROM ATA status register.

--- UniPCemu/hardware/ide.c
+++ UniPCemu/hardware/ide.c
@@ -3040,13 +3040,19 @@ OPTINLINE void ATA_updateStatus(byte channel)
 	switch (ATA[channel].Drive[ATA_activeDrive(channel)].commandstatus) //What command status?
 	{
 	case 0: //Ready for command?
-		ATA_STATUSREGISTER_BUSYW(channel,ATA_activeDrive(channel),(((ATA[channel].Drive[ATA_activeDrive(channel)].ATAPI_PendingExecuteTransfer && (ATA[channel].Drive[ATA_activeDrive(channel)].ATAPI_processingPACKET<3 /* 3(result)/4(pending result status) clear busy */)))||DRIVECONTROLREGISTER_SRSTR(channel)||(ATA[channel].Drive[ATA_activeDrive(channel)].resetTiming))?1:0); //Not busy! You can write to the CBRs! We're busy during the ATAPI transfer still pending the result phase! Result phase pending doesn't set it!
+		ATA_STATUSREGISTER_BUSYW(channel,ATA_activeDrive(channel),((((ATA[channel].Drive[ATA_activeDrive(channel)].ATAPI_PendingExecuteTransfer && (ATA[channel].Drive[ATA_activeDrive(channel)].ATAPI_processingPACKET<3 /* 3(result)/4(pending result status) clear busy */)))||DRIVECONTROLREGISTER_SRSTR(channel)||(ATA[channel].Drive[ATA_activeDrive(channel)].resetTiming))?1:0)&(ATA[channel].Drive[ATA_activeDrive(channel)].PARAMETERS.reportReady)); //Not busy! You can write to the CBRs! We're busy during the ATAPI transfer still pending the result phase! Result phase pending doesn't set it!
 		ATA_STATUSREGISTER_DRIVEREADYW(channel,ATA_activeDrive(channel),(((((ATA[channel].driveselectTiming||ATA[channel].Drive[ATA_activeDrive(channel)].ReadyTiming) && (ATA[channel].Drive[ATA_activeDrive(channel)].ATAPI_processingPACKET<4 /* 4(pending result status) sets ready */)) || (ATA[channel].Drive[ATA_activeDrive(channel)].IRQTimeout))||DRIVECONTROLREGISTER_SRSTR(channel))?0:1)&(ATA[channel].Drive[ATA_activeDrive(channel)].PARAMETERS.reportReady)); //We're ready to process a command!
 		ATA_STATUSREGISTER_DRIVEWRITEFAULTW(channel,ATA_activeDrive(channel),0); //No write fault!
 		ATA_STATUSREGISTER_DATAREQUESTREADYW(channel,ATA_activeDrive(channel),0); //We're requesting data to transfer!
 		if (ATA_Drives[channel][ATA_activeDrive(channel)] < CDROM0) //Hard disk?
 		{
 			ATA_STATUSREGISTER_DRIVESEEKCOMPLETEW(channel,ATA_activeDrive(channel),1); //Not seeking anymore, since we're ready to run!
+		}
+		if (ATA[channel].Drive[ATA_activeDrive(channel)].PARAMETERS.reportReady == 0) //Reporting non-existant?
+		{
+			ATA_STATUSREGISTER_ERRORW(channel, ATA_activeDrive(channel), 0); //Nothing!
+			ATA_STATUSREGISTER_INDEXW(channel, ATA_activeDrive(channel), 0); //Nothing!
+			ATA_STATUSREGISTER_CORRECTEDDATAW(channel, ATA_activeDrive(channel), 0); //Nothing!
 		}
 		break;
 	case 1: //Transferring data IN?
