2f800f24c0a7c0e8df9a0f288fd6a3e409cf2077
superfury <superfury@live.nl>
2019-06-26 16:28:25 +0200

    - Improved ATA/ATAPI error reporting to be correctly reset. - Improved ATA reset to set the error register properly to 1 when needed after a reset.

--- UniPCemu/hardware/ide.c
+++ UniPCemu/hardware/ide.c
@@ -2670,13 +2670,17 @@ void ATA_reset(byte channel, byte slave)
 	{
 		slave &= 0x7F;
 		ATA[channel].Drive[slave].PARAMETERS.reportReady = 1; //Report ready now!
+		if ((slave&0x80) || ((ATA_STATUSREGISTER_ERRORR(channel,slave)==0) && ((slave&0x80)==0))) //Pending error for these drives?
+			ATA[channel].Drive[slave].ERRORREGISTER = 0x00; //No error, but being a reserved value of 0 usually!
+		else //SRST reset with error left to handle?
+			ATA[channel].Drive[slave].ERRORREGISTER = 0x01; //No error, but being a reserved value of 1 usually!
 	}
 	else
 	{
 		slave &= 0x7F;
 		ATA[channel].Drive[slave].PARAMETERS.reportReady = 0; //Report not ready now!
+		ATA[channel].Drive[slave].ERRORREGISTER = 0x00; //No error, but being a reserved value of 0 usually!
 	}
-	ATA[channel].Drive[slave].ERRORREGISTER = 0x01; //No error, but being a reserved value of 1!
 	//Clear Drive/Head register, leaving the specified drive as it is!
 	ATA_DRIVEHEAD_HEADW(channel,slave,0); //What head?
 	ATA_DRIVEHEAD_LBAMODE_2W(channel,slave,0); //LBA mode?
@@ -3145,9 +3149,9 @@ OPTINLINE void ATA_updateStatus(byte channel)
 		{
 			ATA_STATUSREGISTER_DRIVESEEKCOMPLETEW(channel,ATA_activeDrive(channel),1); //Not seeking anymore, since we're ready to run!
 		}
+		ATA_STATUSREGISTER_ERRORW(channel, ATA_activeDrive(channel), 0); //No error!
 		if (ATA[channel].Drive[ATA_activeDrive(channel)].PARAMETERS.reportReady == 0) //Reporting non-existant?
 		{
-			ATA_STATUSREGISTER_ERRORW(channel, ATA_activeDrive(channel), 0); //Nothing!
 			ATA_STATUSREGISTER_INDEXW(channel, ATA_activeDrive(channel), 0); //Nothing!
 			ATA_STATUSREGISTER_CORRECTEDDATAW(channel, ATA_activeDrive(channel), 0); //Nothing!
 		}
