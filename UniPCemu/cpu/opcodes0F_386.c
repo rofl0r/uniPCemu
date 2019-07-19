b519a5a5c17d0fdf30684364ecafd5bc82663da2
superfury <superfury@live.nl>
2019-07-19 19:22:05 +0200

    - Fixed a code bug with LOADALL loading data from memory.

--- UniPCemu/cpu/opcodes0F_386.c
+++ UniPCemu/cpu/opcodes0F_386.c
@@ -571,7 +571,7 @@ byte LOADALL386_checkMMUaccess(word segment, uint_64 offset, byte readflags, byt
 		}
 	}
 
-	if (((readflags & 0x40) == 0) && (segment!=-128))
+	if ((readflags & 0x40) == 0)
 	{
 		if (checkDirectMMUaccess(realaddress, readflags, CPL)) //Failure in the Paging Unit?
 		{
