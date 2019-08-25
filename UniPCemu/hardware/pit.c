7ccf70eab0b963c7b4c83dec13bbfb754d1752dd
superfury <superfury@live.nl>
2019-08-25 17:47:08 +0200

    - Changed the UniPCemu license to GPL.

--- UniPCemu/hardware/pit.c
+++ UniPCemu/hardware/pit.c
@@ -1,4 +1,21 @@
 /*
+This file is part of UniPCemu.
+
+UniPCemu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UniPCemu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UniPCemu.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
 
 src:http://wiki.osdev.org/Programmable_Interval_Timer#Channel_2
 82C54 PIT (Timer) (82C54(8253/8254 in older systems))
