372eac59c621754f8ee9a14ac2227df61d13d2cb
superfury <superfury@live.nl>
2019-12-05 02:25:51 +0100

    - Changed the modem PnP Device ID and produce ID to a more generic setting.

--- UniPCemu/hardware/modem.c
+++ UniPCemu/hardware/modem.c
@@ -1047,8 +1047,8 @@ void MODEM_sendAutodetectionPNPmessage()
 	//According to https://git.kontron-electronics.de/linux/linux-imx-exceet/blob/115a57c5b31ab560574fe1a09deaba2ae89e77b5/drivers/serial/8250_pnp.c , PNPC10F should be a "Standard Modem".
 	//"PNPC10F"=Standard Modem. Order is(in order of escapes: Version(two 5-bits values, divided by 100 is the version number, high 5-bits first, low 5-bits second) ID("PNP"), product ID(the ID), Serial number(00000001), Class name("MODEM", as literally in the Plug and Play Exernal COM Device Specification Version 1.00 February 28, 1995), Device ID("," followed by the ID), User name("Modem", this is what's reported to the user as plain text).
 	//The ID used to be "PNPC10F". Use PNPC102 for a safe Standard 28800bps modem.
-	char EISA_productID[] = "PNPC102"; //Product ID!
-	char DeviceID[] = ",PNPC102"; //Device ID!
+	char EISA_productID[] = "PNPC107"; //Product ID! Standard modem?
+	char DeviceID[] = ",PNPC107"; //Device ID! Standard modem?
 	char PNPHeader[] = "\x28\x01\x24"; //Header until EISA/product ID
 	char PNPMid[] = "\\00000001\\MODEM"; //After EISA/product ID until Device ID
 	char PNPFooter[] = "\\ModemCC\x29"; //Footer with checksum!
