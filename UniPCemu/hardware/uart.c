3529f9e2bd35358915de623af25b8c1497fe9f41
superfury <superfury@live.nl>
2020-04-21 09:59:35 +0200

    - Fixed the UART IIR interrupt request cause to be compatible with the other causes.

--- UniPCemu/hardware/uart.c
+++ UniPCemu/hardware/uart.c
@@ -305,7 +305,7 @@ byte PORT_readUART(word port, byte *result) //Read from the uart!
 			break;
 		case 2: //Interrupt ID registers?
 			*result = UART_port[COMport].InterruptIdentificationRegister&(~0xE0); //Give the register! Indicate no FIFO!
-			if ((!UART_INTERRUPTIDENTIFICATIONREGISTER_INTERRUPTNOTPENDINGR(COMport)) && ((UART_INTERRUPTCAUSE_SIMPLECAUSER(COMport) == 1) || (UART_INTERRUPTCAUSE_SIMPLECAUSER(COMport) == IRR_INTERRUPTREQUEST_CAUSE))) //We're to clear?
+			if ((!UART_INTERRUPTIDENTIFICATIONREGISTER_INTERRUPTNOTPENDINGR(COMport)) && ((UART_INTERRUPTCAUSE_SIMPLECAUSER(COMport) == 1) || ((UART_INTERRUPTCAUSE_SIMPLECAUSER(COMport) == IRR_INTERRUPTREQUEST_CAUSE) && (IRR_INTERRUPTREQUEST_CAUSE>3)))) //We're to clear?
 			{
 				UART_port[COMport].InterruptIdentificationRegister = 0; //Reset the register!
 				UART_INTERRUPTIDENTIFICATIONREGISTER_INTERRUPTNOTPENDINGW(COMport,1); //Reset interrupt pending!
