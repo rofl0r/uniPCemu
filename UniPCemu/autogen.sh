8c578cb2bb520a9c81f95d49253befb001904cce
superfury <superfury@live.nl>
2020-04-24 12:19:51 +0200

    - Improved automatic ignoring of the newly generated Makefile when running autogen.sh.

--- UniPCemu/autogen.sh
+++ UniPCemu/autogen.sh
@@ -1,2 +1,4 @@
+git update-index --assume-unchanged Makefile
 aclocal || die "aclocal failed" # Set up an m4 environment
 autoconf || die "autoconf failed" # Generate configure from configure.ac
+echo Configure script is generated.
