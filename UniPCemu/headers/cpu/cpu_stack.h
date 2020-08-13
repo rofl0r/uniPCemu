23ecaa21bcdaf41cb937a83c6c022d66bdbd593b
superfury <superfury@live.nl>
2020-08-13 08:46:03 +0200

    - Added missing copyright clauses to various headers and source code files. - Added missing license information to various newer files.

--- UniPCemu/headers/cpu/cpu_stack.h
+++ UniPCemu/headers/cpu/cpu_stack.h
@@ -1,3 +1,23 @@
+/*
+
+Copyright (C) 2020  Superfury
+
+This file is part of UniPCemu.
+
+UniPCemu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UniPCemu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UniPCemu.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef CPU_STACK_H
 #define CPU_STACK_H
 
